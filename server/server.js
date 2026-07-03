dccbab2751db6f5b401a3a9383c2223a9d08ed32
anaga2005 <anupamaravala@gmail.com>
2026-07-03 19:18:32 +0530

    Initial project setup

---
+++ server/server.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./db/config");
+
+const app = express();
+
+// Connect to MongoDB
+connectDB();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Test Route
+app.get("/", (req, res) => {
+  res.send("Nutrition Assistant API Running");
+});
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
